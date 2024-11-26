48ac107b2555137a4f2732b454bd4b3344339de4
trinebra <157691390+TrineBraa@users.noreply.github.com>
2024-11-26 14:26:15 +0100

    Tailwindcss issues
    Sorted out the Tailwind issue so the css works as it should.

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,7 +1,7 @@
 function NavBar(){
 return (
     <>
-        <div className="absolute inset-x-0 top-0 display-flex justify-content-center bg-white">
+        <div className="">
         <h1 className="font-serif text-3xl">Welcome</h1>
         <h3>This is your personal library!</h3>
         <button className="">Find books</button> 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js, ts, tsx, jsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
