24083735b6b74e42388a8f6a0c6ecbcf235fa96d
trinebra <157691390+TrineBraa@users.noreply.github.com>
2024-12-02 13:18:23 +0100

    Modal and CSS for it
    Set up the modal with CSS for more info on each book if you press the more info button

--- frontend/src/components/displayMoreInfo.tsx
+++ frontend/src/components/displayMoreInfo.tsx
@@ -1,5 +1,6 @@
 import { Book } from "./bookInfo";
 
+
 interface DisplayMoreInfo {
  book: Book;
  closeMoreInfo: () => void
@@ -7,10 +8,19 @@ interface DisplayMoreInfo {
 
 const DisplayMoreInfo: React.FC<DisplayMoreInfo> = ({book, closeMoreInfo}) => {
   return (
-    <div>
-      <h2>{book.title}</h2>
-      <button onClick={closeMoreInfo}>Close</button>
+    <div className="modal">    
+      <div className="modalContent">
+      <button className="butnClose bg-cyan-900 border px-1 mx-2 text text-white hover:bg-teal-950 " onClick={closeMoreInfo}>Close</button>
+      <h2 className="text-3xl ml-48 my-1 bold">{book.title}</h2>
+      <img src={book.imageLinks?.thumbnail || "placeholder"} alt={book.title}/>
+      <p className="mb-2">Published: {book.publishedDate || "No publication Date"}</p>
+      <p>Author: {book.authors?.join(",") || "Unknown Author"}</p>
+      <p className="pb-5">Description: {book.description || "No description"}</p>
+      
+      <button className=" bg-cyan-900 border px-1 mx-2 text text-white hover:bg-teal-950 " onClick={closeMoreInfo}>Close</button>
+      <button className="bg-cyan-900 border px-1 mx-2 text text-white hover:bg-teal-950 ">Add to library</button>
     </div>
+  </div>
   )
 }
 
